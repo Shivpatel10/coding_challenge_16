ba31ba0199729983a7453b51e99f54e07406917f
Shiv Patel <shivpatel0204@gmail.com>
2024-11-15 19:11:17 -0500

    .

---
+++ src/components/ProductListComponent.jsx
@@ -0,0 +1,26 @@
+//import React from 'react';
+
+const products = [
+    {id: 1, name: 'IPhone', price: 1000 , description: "A compact handheld device to used to communicate over long distances and complete daily tasks."},
+    {id: 2, name: 'MacBook', price: 1200, description: "A portable machine used to complete more complex tasks on a bigger screen."},
+    {id: 3, name: 'AirPod Pros', price: 200, description: "A set of wireless earbuds that are portable and have noise cancelling capabilites."},
+    {id: 4, name: 'AirPod Max', price: 450, description: "A set of wireless over the head headphones that provide the highest quality of sound."},
+    {id: 5, name: 'Apple Watch', price: 250, description: "A wearable wristwatch that can track and log health and fitnessinformation."}
+]
+
+function ProductList() { //Created an un orded list from my array above
+  return (
+  <div>
+      <h2> Product List</h2>
+      <ul>
+          {products.map(product => (
+              <li key={product.id} > 
+                    {product.name} - ${product.price}
+              </li>
+          ))}
+      </ul>
+  </div>
+ );
+}
+
+export default ProductList;
